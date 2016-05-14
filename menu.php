091cdf0246e3419b235104a5ce5f06043cad832c
kaisabekov <tathebesthero@gmail.com>
2016-05-14 23:25:38 +0600

    testing

--- menu.php
+++ menu.php
@@ -6,7 +6,7 @@
     background-image: url("images/img13a.jpg");
     background-repeat:no-repeat;
     background-color:#C2D1F0;
-    background-size: 1367px auto;
+		background-size: 100% 100%
     }
    a.menu{
       color:red;
@@ -29,7 +29,6 @@
 	font-family: Goudy Old Style,Garamond,Big Caslon,Times New Roman,serif;
 	z-index: 1;
 	opacity:0.7;
-	position:fixed;
     background-color:#F0E0FF;
     font-color:black;
     margin:10px;
@@ -51,7 +50,7 @@
     float:right;
     padding:5px; position:relative;
     top:110px;
-    left:10px;     
+    left:10px;
 }
   pre.sky{
     position:fixed;
@@ -70,18 +69,17 @@
   <li ><A HREF="gallery.php?">&nbsp Gallery &nbsp </A></li>
   <li >&nbsp  </li>
   <li ><A HREF="menu.php?" class="menu">&nbsp Menu &nbsp</A></li>
- <li > &nbsp  </li> 
+ <li > &nbsp  </li>
   <li ><A HREF="tableBooking.php?">&nbsp Table Booking &nbsp</A></li>
- <li >   </li> 
+ <li >   </li>
   <li ><A HREF="bookingStatus.php?">&nbsp Booking Status &nbsp</A></li>
- <li >  </li> 
+ <li >  </li>
   <li ><A HREF="comment.php?">&nbsp Visitor's Comment &nbsp</A></li>
- <li >&nbsp </li> 
+ <li >&nbsp </li>
   <li ><A HREF="contactUs.php?">&nbsp Contact Us &nbsp</A></li>
 </ul>
 </div>
-<!--<marquee>HOTEL LA VILLA IN THE SKY</marquee>-->
-<pre class="sky1"><nav style="left:-1059;font-size: 22px;top:130px;"> <A HREF="category.php?id=1" target="iframe">     Best Secret Menu Items     </A>
+<pre class="sky1"><nav style="left:-40%;font-size: 22px;"> <A HREF="category.php?id=1" target="iframe">     Best Secret Menu Items     </A>
  <A HREF="category.php?id=2" target="iframe">              Beverages                   </A>
 
  <A HREF="category.php?id=3" target="iframe">                Snacks                     </A>
@@ -96,29 +94,7 @@
 
  <A HREF="category.php?id=8" target="iframe">        Main Course - Veg         </A>
 <A HREF="category.php?id=9" target="iframe">      Main Course - Non Veg     </A>
-
-<--  LA VILLA IN THE SKY  -->
-</nav></pre>
-<pre class="sky"><nav style="left:1025;font-size: 20px;">
-
-   We welcome you to a delicious 
-      feast of exquisite dishes 
-           in The Sky.
-
-     With a wide range of 
-        world cuisines 
-        to choose from,
-
-   we guarantee you a sumptuous 
-       feast experience in 
-         our restaurant.
-
-
-  <-- LA VILLA IN THE SKY -->   
-
-<iframe name="iframe" src="images/img9a.jpg"width="712" height="505" style="position:relative;right:730px;top:-513px;opacity=;background-color:white;"></iframe>
 </nav></pre>
 
 </body>
 </html>
-
