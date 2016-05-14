302ee954a8665347d08a4e0261b2fe463350319c
kaisabekov <tathebesthero@gmail.com>
2016-05-14 23:24:52 +0600

    testing

--- bookingStatus.php
+++ bookingStatus.php
@@ -29,7 +29,6 @@
 	font-family: Goudy Old Style,Garamond,Big Caslon,Times New Roman,serif;
 	z-index: 1;
 	opacity:0.7;
-	position:fixed;
     background-color:#F0E0FF;
     font-color:black;
     margin:10px;
@@ -51,7 +50,7 @@
     float:right;
     padding:5px; position:relative;
     top:110px;
-    left:10px;     
+    left:10px;
 }
   pre.sky{
     position:fixed;
@@ -70,41 +69,18 @@
   <li ><A HREF="gallery.php?">&nbsp Gallery &nbsp </A></li>
   <li >&nbsp  </li>
   <li ><A HREF="menu.php?" >&nbsp Menu &nbsp</A></li>
- <li > &nbsp  </li> 
+ <li > &nbsp  </li>
   <li ><A HREF="tableBooking.php?">&nbsp Table Booking &nbsp</A></li>
- <li >   </li> 
+ <li >   </li>
   <li ><A HREF="bookingStatus.php?" class="bookStatus">&nbsp Booking Status &nbsp</A></li>
- <li >  </li> 
+ <li >  </li>
   <li ><A HREF="comment.php?">&nbsp Visitor's Comment &nbsp</A></li>
- <li >&nbsp </li> 
+ <li >&nbsp </li>
   <li ><A HREF="contactUs.php?">&nbsp Contact Us &nbsp</A></li>
 </ul>
 </div>
-<!--<marquee>HOTEL LA VILLA IN THE SKY</marquee>-->
-<pre class="sky1"><nav style="left:-1059;font-size: 22px;top:130px;opacity:1;"><img src="images/img3.jpg" width=270px height=auto>
-
-<--  LA VILLA IN THE SKY  -->
 </nav></pre>
-<pre class="sky"><nav style="left:1025;font-size: 20px;opacity:0.8;">
-
-   We welcome you to a delicious 
-      feast of exquisite dishes 
-           in The Sky.
-
-     With a wide range of 
-        world cuisines 
-        to choose from,
-
-   we guarantee you a sumptuous 
-       feast experience in 
-         our restaurant.
-
-
-  <-- LA VILLA IN THE SKY -->   
-
-<iframe name="iframe" src="enterUr_id.php?" width="712" height="505" style="position:relative;right:730px;top:-513px;background-color:white;"></iframe>
-</nav></pre>
+<iframe name="iframe" src="enterUr_id.php?" width="712" height="505" style="margin-left: 30% ;top:40%;background-color:white;"></iframe>
 
 </body>
 </html>
-
